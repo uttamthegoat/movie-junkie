360f2916ec47c6d2f05ecc8bc63712689ab030aa
uttamthegoat <19516uttam@gmail.com>
2023-06-20 15:49:50 +0530

    Implemented code splitting and added redirect page

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,58 @@
 import "./App.css";
+import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import requests from "./sampleData";
 import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Movies from "./components/Movies";
-import MovieDetails from "./components/MovieDetails";
-import TvDetails from "./components/TvDetails";
-import Search from "./components/Search";
+// import Home from "./components/Home";
+// import Movies from "./components/Movies";
+// import MovieDetails from "./components/MovieDetails";
+// import TvDetails from "./components/TvDetails";
+// import Search from "./components/Search";
+
+const Home = lazy(() => import("./components/Home"));
+const Movies = lazy(() => import("./components/Movies"));
+const MovieDetails = lazy(() => import("./components/MovieDetails"));
+const TvDetails = lazy(() => import("./components/TvDetails"));
+const Search = lazy(() => import("./components/Search"));
+const Redirect = lazy(() => import("./components/Redirect"));
 
 function App() {
   return (
     <div className="App">
-      <ScrollToTop />
-      <Navbar title="Movie Junkie" />
-      <Routes>
-        <Route exact path="/" element={<Home title="Home" />} />
-        <Route
-          exact
-          path="/movies"
-          element={
-            <Movies
-              key="movie"
-              title="Movies"
-              fetchContent={requests.fetchMovies}
-            />
-          }
-        />
-        <Route
-          exact
-          path="/tv_series"
-          element={
-            <Movies
-              key="tv"
-              title="TV Series"
-              fetchContent={requests.fetchTvSeries}
-            />
-          }
-        />
-        <Route exact path="/movie_details" element={<MovieDetails />} />
-        <Route exact path="/tv_details" element={<TvDetails />} />
-        <Route exact path="/search" element={<Search />} />
-      </Routes>
+      <Suspense fallback={<h1 className="text-center">Loading...</h1>}>
+        <ScrollToTop />
+        <Navbar title="Movie Junkie" />
+        <Routes>
+          <Route exact path="/" element={<Home title="Home" />} />
+          <Route
+            exact
+            path="/movies"
+            element={
+              <Movies
+                key="movie"
+                title="Movies"
+                fetchContent={requests.fetchMovies}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/tv_series"
+            element={
+              <Movies
+                key="tv"
+                title="TV Series"
+                fetchContent={requests.fetchTvSeries}
+              />
+            }
+          />
+          <Route exact path="/movie_details" element={<MovieDetails />} />
+          <Route exact path="/tv_details" element={<TvDetails />} />
+          <Route exact path="/search" element={<Search />} />
+          <Route path="*" element={<Redirect />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/Redirect.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Redirect() {
+  return (
+    <div className="container">
+      <h1 className="text-center my-4">404 Error</h1>
+      <hr />
+      <div className="text-center">
+        <h2>Page not found</h2>
+        <Link to="/" className="mx-auto text-primary" style={{textDecoration:"none"}}>
+          Go back to Home Page
+        </Link>
+      </div>
+    </div>
+  );
+}
