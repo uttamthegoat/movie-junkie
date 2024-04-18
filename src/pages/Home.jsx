e9a9c690408e3d495f86feecdad45985601e6815
uttamthegoat <19516uttam@gmail.com>
2024-04-19 00:13:13 +0530

    Change Check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ import "aos/dist/aos.css";
 export default function Home() {
   useEffect(() => {
     AOS.init({ duration: 1000 });
-    document.title = "Movie Junkie - Home";
+    document.title = "Movie Junkie";
   }, []);
   return (
     <div className="Home">
