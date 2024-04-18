5e5c2d7c688690d980d298f74484e896c7325ab9
uttamthegoat <19516uttam@gmail.com>
2024-04-19 00:05:29 +0530

    Local and remote merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import "./App.css";
 import React, { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import requests from "./sampleData";
 import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/Navbar";
+// import Home from "./components/Home";
+// import Movies from "./components/Movies";
+// import MovieDetails from "./components/MovieDetails";
+// import TvDetails from "./components/TvDetails";
+// import Search from "./components/Search";
 
-import Home from "./pages/Home";
-import Movies from "./pages/Movies";
-import MovieDetails from "./pages/MovieDetails";
-import TvDetails from "./pages/TvDetails";
-import Search from "./pages/Search";
-
-// const Home = lazy(() => import("./pages/Home"));
-// const Movies = lazy(() => import("./pages/Movies"));
-// const MovieDetails = lazy(() => import("./pages/MovieDetails"));
-// const TvDetails = lazy(() => import("./pages/TvDetails"));
-// const Search = lazy(() => import("./pages/Search"));
+const Home = lazy(() => import("./pages/Home"));
+const Movies = lazy(() => import("./pages/Movies"));
+const MovieDetails = lazy(() => import("./pages/MovieDetails"));
+const TvDetails = lazy(() => import("./pages/TvDetails"));
+const Search = lazy(() => import("./pages/Search"));
 const Redirect = lazy(() => import("./pages/Redirect"));
 
 function App() {
   return (
-    <BrowserRouter basepath="/">
     <div className="App">
       <Suspense fallback={<h1 className="text-center">Loading...</h1>}>
         <ScrollToTop />
@@ -28,6 +26,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home title="Home" />} />
           <Route
+            exact
             path="/movies"
             element={
               <Movies
@@ -38,6 +37,7 @@ function App() {
             }
           />
           <Route
+            exact
             path="/tv_series"
             element={
               <Movies
@@ -48,16 +48,16 @@ function App() {
             }
           />
           <Route
+            exact
             path="/movie_details/:source_id"
             element={<MovieDetails />}
           />
-          <Route path="/tv_details/:source_id" element={<TvDetails />} />
-          <Route path="/search" element={<Search />} />
+          <Route exact path="/tv_details/:source_id" element={<TvDetails />} />
+          <Route exact path="/search" element={<Search />} />
           <Route path="*" element={<Redirect />} />
         </Routes>
       </Suspense>
     </div>
-    </BrowserRouter>
   );
 }
 
